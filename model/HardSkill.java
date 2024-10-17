ca96def58a14b72be5c4c559855ad6f784bc2a4f
HeadMaxResh <95633686+HeadMaxResh@users.noreply.github.com>
2024-10-17 23:50:46 +0400

    Create basic main classes and enums

---
+++ model/HardSkill.java
@@ -0,0 +1,18 @@
+package com.t1.profile.model;
+
+import com.t1.profile.enums.HardSkillType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class HardSkill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private HardSkillType type;
+}
