b534e3e5367d6307ac6e5f631a22e3f81cc0fd37
Алексей Берко <aleksej-berko@yandex.ru>
2024-10-19 20:18:54 +0300

    Create a Role model that will represent user roles in the system

---
+++ src/main/java/com/t1/profile/model/Role.java
@@ -0,0 +1,38 @@
+package com.t1.profile.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name; // Например, ROLE_USER, ROLE_ADMIN
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    // Геттеры и сеттеры
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Остальные геттеры и сеттеры
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
