fe8e70b9d23da2e3cc20b14dd75bdfe5ea6093f9
Алексей Берко <aleksej-berko@yandex.ru>
2024-10-19 20:16:31 +0300

    Updating the user model

--- src/main/java/com/t1/profile/model/User.java
+++ src/main/java/com/t1/profile/model/User.java
@@ -1,39 +1,58 @@
 package com.t1.profile.model;
 
-import com.t1.profile.Table;
-import jakarta.persistence.*;
-import lombok.Data;
-
-import java.time.LocalDate;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
-@Entity(name = Table.TABLE_USER)
+@Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String firstName;
-    private String lastName;
-    private LocalDate dateOfBirth;
-    private String gender;
-    private String city;
-    @Column(unique = true)
+
+    private String name;
+
     private String email;
-    private String passwordHash;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<HardSkill> mainHardSkills = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<HardSkill> additionalHardSkills = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<SoftSkill> softSkills = new HashSet<>();
+    private String password; // Добавляем поле для пароля
 
     @ManyToOne
     @JoinColumn(name = "profession_id")
     private Profession profession;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles", // Предполагая, что у нас есть таблица user_roles
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>(); // Добавляем роли пользователя
+
+    // Геттеры и сеттеры
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    // Остальные геттеры и сеттеры
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
