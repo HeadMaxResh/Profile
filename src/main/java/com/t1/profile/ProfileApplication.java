09f24bfab001ed4d664bda9346bc65707ebc1a90
alexey-skoblin <alexey.skoblin@gmail.com>
2024-11-03 00:36:00 +0400

    Base work configuration

--- src/main/java/com/t1/profile/ProfileApplication.java
+++ src/main/java/com/t1/profile/ProfileApplication.java
@@ -2,8 +2,14 @@ package com.t1.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableFeignClients
 public class ProfileApplication {
 
 	public static void main(String[] args) {
